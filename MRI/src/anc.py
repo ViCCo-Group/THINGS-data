e9e60e3170ee26418e73a2255caecb179eb74e6e
Oliver Contier <o.contier@gmail.com>
2022-12-12 13:53:12 +0100

    Made ANC cli accessible

--- MRI/src/anc.py
+++ MRI/src/anc.py
@@ -1,5 +1,11 @@
 """
-Calculate analytical noise ceiling.
+Calculate the noise ceiling in single trial response estimates.
+
+Usage:
+python anc.py <bids_path> <subject_ID> <betas_path> <output_path>
+
+Example:
+python anc.py 01 /home/user/thingsmri /home/user/thingsmri/betas_vol /home/user/thingsmri/noiseceiling
 """
 
 import os
@@ -9,7 +15,7 @@ from os.path import pardir
 from tqdm import tqdm
 import numpy as np
 from scipy.stats import zscore
-from nilearn.image import concat_imgs, mean_img, threshold_img, math_img, index_img, new_img_like
+from nilearn.image import new_img_like
 
 sys.path.append(os.getcwd())
 from betas import list_stb_outputs_for_mcnc
@@ -50,65 +56,37 @@ def kknc(data: np.ndarray, n: int or None = None, ignore_nans=False):
 def calc_kknc_singletrialbetas(
         sub: str,
         bidsroot: str,
-        stb_derivnames: list,
-        out_derivnames: list,
+        betas_basedirs: list,
+        out_dirs: list,
         ns: list,
 ) -> None:
     """
     Calculate KKNC for different versions of our single trial betas.
     Example:
-        sub = sys.argv[1]
-        bidsroot = pjoin(pardir, pardir, pardir, pardir)
-        ns = [1, 3, 12]
-        stb_derivnames = [
-            'derivatives/betas_loo/on_residuals/assume_hrf_unregularized',
-            'derivatives/betas_loo/on_residuals/unregularized',
-            'derivatives/betas_loo/on_residuals/regularized',
-            'derivatives/betas_loo/on_residuals/scalematched',
-        ]
-        out_derivnames = [f.replace('/betas_loo/', '/kknc/model-stb/') for f in stb_derivnames]
+        sub = '01'
+        bidsroot = '/path/to/bids/dataset'
+        ns = [1, 12]
+        stb_derivnames = ['derivatives/betas_vol/']
+        out_derivnames = ['derivatives/nc/']
         calc_kknc_singletrialbetas(sub, bidsroot, stb_derivnames, out_derivnames, ns)
     """
-    assert len(stb_derivnames) == len(out_derivnames)
-    for stb_derivname, out_derivname in tqdm(zip(stb_derivnames, out_derivnames),
-                                             desc='Iterating through betas versions', total=len(stb_derivnames)):
-        betas_basedir = pjoin(bidsroot, 'derivatives', stb_derivname)
+    assert len(betas_basedirs) == len(out_dirs)
+    for betas_basedir, outdir in tqdm(zip(betas_basedirs, out_dirs), desc='Iterating through betas versions', total=len(out_dirs)):
         assert os.path.exists(betas_basedir)
-        outdir = pjoin(bidsroot, 'derivatives', out_derivname)
         if not os.path.exists(outdir):
-            print(f'making output directory: {outdir}')
             os.makedirs(outdir)
-        print(f'loading betas: {stb_derivname}')
         betas, example_img = list_stb_outputs_for_mcnc(sub, bidsroot, betas_basedir=betas_basedir)
         ncs = [kknc(betas, n) for n in tqdm(ns, desc='calculating noise ceilings')]
         for n, nc in tqdm(zip(ns, ncs), desc='saving output files', total=len(ncs)):
             img = new_img_like(example_img, nc)
-            outfile = pjoin(bidsroot, 'derivatives', out_derivname, f'sub-{sub}_kknc_n-{n}.nii.gz')
+            outfile = pjoin(outdir, f'sub-{sub}_kknc_n-{n}.nii.gz')
             img.to_filename(outfile)
 
 
-if __name__ == '__main__':
-    sub = sys.argv[1]
-    bidsroot = pjoin(pardir, pardir, pardir, pardir)
-    ns = list(range(1, 13))
-    stb_derivnames = [
-        'betas_loo/on_residuals/assume_hrf_unregularized',
-        'betas_loo/on_residuals/unregularized',
-        'betas_loo/on_residuals/regularized',
-        'betas_loo/on_residuals/scalematched',
-        'betas_loo/simultaneous_fit/assume_hrf_unregularized',
-        'betas_loo/simultaneous_fit/unregularized',
-        'betas_loo/simultaneous_fit/regularized',
-        'betas_loo/simultaneous_fit/scalematched',
-        'betas_loo/orthogonalized/assume_hrf_unregularized',
-        'betas_loo/orthogonalized/unregularized',
-        'betas_loo/orthogonalized/regularized',
-        'betas_loo/orthogonalized/scalematched',
-        'betas_loo/no_icadenoising/assume_hrf_unregularized',
-        'betas_loo/no_icadenoising/unregularized',
-        'betas_loo/no_icadenoising/regularized',
-        'betas_loo/no_icadenoising/scalematched',
-        'betas_loo/aroma/on_residuals/assume_hrf_unregularized',
-    ]
-    out_derivnames = [f.replace('betas_loo/', 'kknc/model-stb_nanvar/') for f in stb_derivnames]
-    calc_kknc_singletrialbetas(sub, bidsroot, stb_derivnames, out_derivnames, ns)
+if __name__=='__main__':
+    sub, bidsroot, betaspath, outpath = sys.argv[1], sys.argv[2]
+    ns = [1, 12]
+    betas_basedirs = [betaspath]
+    out_dirs = [outpath]
+    calc_kknc_singletrialbetas(sub, bidsroot, betas_basedirs, out_dirs, ns)
+        
